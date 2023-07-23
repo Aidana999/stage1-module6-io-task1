144cb62b4cf3209653b16681381f2df65673ef35
Aidana <49874037+Aidana999@users.noreply.github.com>
2023-07-23 12:03:33 +0400

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,6 +1,12 @@
 package com.epam.mjc.io;
 
+//import java.io.File;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
